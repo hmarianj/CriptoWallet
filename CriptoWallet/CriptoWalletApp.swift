b149bd4f5950643b3d9c8972d5f66280580dc9f7
Marian <hmarianj25@gmail.com>
2025-03-17 10:57:53 -0300

    Initial Commit

---
+++ CriptoWallet/CriptoWalletApp.swift
@@ -0,0 +1,32 @@
+//
+//  CriptoWalletApp.swift
+//  CriptoWallet
+//
+//  Created by MH on 17/03/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CriptoWalletApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
